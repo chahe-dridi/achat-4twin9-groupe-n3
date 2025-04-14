3a476728db9e86cce4852293c338cd05b94bb2fc
chahe-dridi <chaher.dridi.100@gmail.com>
2025-04-14 10:54:21 +0100

    fixed app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductsComponent } from './products/products.component';
-import { NgbModal, NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { StockComponent } from './stock/stock.component';
@@ -13,7 +12,7 @@ import { SecteurActiviteComponent } from './secteur-activite/secteur-activite.co
 import { OperateurComponent } from './operateur/operateur.component';
 import { FactureComponent } from './facture/facture.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';  // Importer ngx-logger
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
@@ -32,16 +31,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     NgbModalModule,
-    RouterModule,
     NgbModule,
+    RouterModule,
     LoggerModule.forRoot({
-      level: NgxLoggerLevel.DEBUG,  // Niveau de log minimum
-      serverLogLevel: NgxLoggerLevel.ERROR, // Niveau pour les logs envoyés au serveur
-      disableConsoleLogging: false // S'assurer que les logs ne sont pas désactivés
+      level: NgxLoggerLevel.DEBUG,
+      serverLogLevel: NgxLoggerLevel.ERROR,
+      disableConsoleLogging: false
     }),
-    FormsModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
